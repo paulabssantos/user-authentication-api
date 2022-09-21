ab7a654494393571cdf39c957b102d268ed13ee0
paulabssantos <paulablsantos7@gmail.com>
2022-09-21 16:20:23 -0400

    inicial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express, {Request,Response,NextFunction} from 'express';
+
+const app = express();
+
+app.get('/status',(req: Request,res: Response,next: NextFunction)=>{
+    res.status(200).send({foo: 'bar'})
+})
+app.listen(3000,()=>{
+    console.log("Server is Running!");  
+})
