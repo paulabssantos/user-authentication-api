40e9d8b0b3731f5a870a3c08967aaae778b7caa3
paulabssantos <paulablsantos7@gmail.com>
2022-09-22 17:29:30 -0400

    refactor: express.json adicionado

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import { routes } from './routes';
 
 const app = express();
 
-app.use(routes);
+app.use(express.json());  
+
+app.use(routes);  
 
 app.listen(3000,()=>{
     console.log("Server is Running!");  
