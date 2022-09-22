39ddc22221fbc32dbd9d8e5f7b8cc5c49d49c67f
paulabssantos <paulablsantos7@gmail.com>
2022-09-22 17:27:43 -0400

    feat: criação de um novo usuário

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -12,4 +12,10 @@ usersRoute.get("/users/:id",(req: Request<{id: number}>,res: Response,next: Next
     res.status(200).send(id);
 })
 
+usersRoute.post("/create",(req: Request<{id: number}>,res: Response,next: NextFunction)=>{
+    const new_user = req.body;
+
+    res.status(201).send(new_user);
+})
+
 export { usersRoute };
